d968487d1a9776221fc50dba4227fd3ba79d1d64
G1lbeert <122795342+G1lbeert@users.noreply.github.com>
2023-05-15 21:46:16 +0200

    Updates

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,51 +1,91 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Location from "./components/Location";
+import NavBar from "./components/NavBar";
 
 function App() {
-
-
   const [allThePokemons, setAllThePokemons] = useState(null);
   const [allTheLocations, setAllTheLocations] = useState(null);
+  const [activeLink, setActiveLink] = useState("#home");
+  const [encounteredPokemon, setEncounteredPokemon] = useState(null);
+  const [showEncounter, setShowEncounter] = useState(false);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1281')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setAllThePokemons(data)
-      })
-    fetch('https://pokeapi.co/api/v2/location?limit=20')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setAllTheLocations(data)
-      })
-  }, [])
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1281")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllThePokemons(data);
+      });
+    fetch("https://pokeapi.co/api/v2/location?limit=20")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllTheLocations(data);
+      });
+  }, []);
 
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    setEncounteredPokemon(null);
+    setShowEncounter(false);
+  };
+
+  const handleLocationClick = (locationURL) => {
+    fetch(locationURL)
+      .then((res) => res.json())
+      .then((data) => {
+        const encounterCount = data.pokemon_encounters.length;
+        if (encounterCount > 0) {
+          const randomIndex = Math.floor(Math.random() * encounterCount);
+          const randomPokemon = data.pokemon_encounters[randomIndex].pokemon;
+          setEncounteredPokemon(randomPokemon);
+        } else {
+          setEncounteredPokemon(null);
+        }
+        setShowEncounter(true);
+      });
+  };
+
+  const handleReturnToLocations = () => {
+    setShowEncounter(false);
+  };
 
   return (
     <div className="App">
-      <div className="topnav">
-        <a href="#location">Location</a>
-        <a href="#pokemons">Pokemons</a>
-        <a href="#pokedex">Pokédex</a>
-        <a className="active" href="#home">Home</a> 
-      </div>
-      <Header/>
-      {allTheLocations && allThePokemons ? (
-        allTheLocations.results.map((location, index) => (
-          <div>
-            {console.log(location.name)}
-          <Location locationName={location.name} locationURL={location.url}/>
-          </div>
-        ))
-      ) : ("")}
-    </div>
-  );
-}
-
-export default App;
+      <NavBar activeLink={activeLink} onLinkClick={handleLinkClick} />
+      <Header />
+      {showEncounter ? (
+        <div>
+          {encounteredPokemon ? (
+            <>
+              <h2>Encountered Pokémon:</h2>
+              <img src={encounteredPokemon.sprites.front_default} alt={encounteredPokemon.name} />
+              <p>Name: {encounteredPokemon.name}</p>
+            </>
+          ) : (
+            <>
+              <p>This location doesn't seem to have any Pokémon.</p>
+              <button onClick={handleReturnToLocations}>Return to Locations</button>
+            </>
+          )}
+        </div>
+      ) : (
+        allTheLocations &&
+        allThePokemons &&
+            allTheLocations.results.map((location, index) => (
+              <div key={index}>
+                {activeLink === "#location" && (
+                  <Location
+                    locationName={location.name}
+                    locationURL={location.url}
+                    onLocationClick={handleLocationClick}
+                  />
+                )}
+              </div>
+            ))
+          )}
+        </div>
+      );
+    }
+    
+    export default App;
---
+++ frontend-app/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function NavBar({ activeLink, onLinkClick }) {
+  return (
+    <>
+      <div className="topnav">
+        <a
+          href="#location"
+          onClick={() => onLinkClick("#location")}
+          className={activeLink === "#location" ? "active" : ""}
+        >
+          Location
+        </a>
+        <a
+          href="#pokemons"
+          onClick={() => onLinkClick("#pokemons")}
+          className={activeLink === "#pokemons" ? "active" : ""}
+        >
+          Pokemons
+        </a>
+        <a
+          href="#pokedex"
+          onClick={() => onLinkClick("#pokedex")}
+          className={activeLink === "#pokedex" ? "active" : ""}
+        >
+          Pokédex
+        </a>
+        <a
+          href="#home"
+          onClick={() => onLinkClick("#home")}
+          className={activeLink === "#home" ? "active" : ""}
+        >
+          Home
+        </a>
+      </div>
+    </>
+  );
+}
